f129c9c4455f68dad9743edf1886df6fe01f935a
extittivns03 <Changhui.Zhang.EXT@desaysv.com>
2023-05-16 14:15:04 +0800

    添加部署脚本

---
+++ deploy/gost.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# gost.sh
+
+# 下面的四个参数需要改成你的
+DOMAIN="YOU.DOMAIN.NAME"
+USER="username"
+PASS="password"
+PORT=443
+
+BIND_IP=0.0.0.0
+CERT_DIR=/etc/letsencrypt
+CERT=${CERT_DIR}/live/${DOMAIN}/fullchain.pem
+KEY=${CERT_DIR}/live/${DOMAIN}/privkey.pem
+sudo docker run -d --name gost \
+    -v ${CERT_DIR}:${CERT_DIR}:ro \
+    --net=host ginuerzh/gost \
+    -L "http2://${USER}:${PASS}@${BIND_IP}:${PORT}?cert=${CERT}&key=${KEY}&probe_resist=code:404&knock=www.google.com"
